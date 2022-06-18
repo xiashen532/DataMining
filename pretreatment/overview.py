3131f08b53b29674f061c1adb4f81b92784577a6
xiashen <245069160@qq.com>
2022-06-18 21:17:15 +0800

    对爬取的二手房数据预处理得到res.csv；增加overview/clean函数去除空值

--- pretreatment/overview.py
+++ pretreatment/overview.py
@@ -7,13 +7,22 @@ import pandas as pd
 def overview(path):
     esf = pd.read_csv(path, encoding='utf-8')
 
-    print(esf['model'].unique())
-    print(esf['direction'].unique())
-    print(esf['fitment'].unique())
-    print(esf['floor'].unique())
-    print(esf['address'].unique())
+    # print(esf['model'].unique())
+    # print(esf['direction'].unique())
+    # print(esf['fitment'].unique())
+    # print(esf['floor'].unique())
+    # print(esf['address'].unique())
     # print(esf[esf.duplicated()])
     print(esf.info())
+    # esf = clean(esf)
+    # esf.to_csv(path, encoding='utf-8', index=False)
 
-path1 = "../data/ershoufang.csv"
+
+# 清除空值
+def clean(file):
+    file.dropna(axis=0, how='any', inplace=True)
+    return file
+
+
+path1 = "../data/res.csv"
 overview(path1)
--- pretreatment/pretreat.py
+++ pretreatment/pretreat.py
@@ -3,12 +3,14 @@ from direction import pre_direction
 from model import pre_model
 from address import pre_address
 from fitment import pre_fitment
+from overview import clean
 import pandas as pd
 
 
 def pretreatment(path):
     file = pd.read_csv(path, encoding='utf-8')
 
+    file = clean(file)
     file = word_to_num(file, 'price_list')
     file = word_to_num(file, 'area')
     file = pre_direction(file)
@@ -18,4 +20,4 @@ def pretreatment(path):
 
     file.to_csv("../data/res.csv", encoding='utf-8', index=False)
 
-pretreatment("../data/test.csv")
+pretreatment("../data/ershoufang.csv")
